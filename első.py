1551cdabb59b28b3153918101395bc5e92514ba4
nagyjuciveronika99 <36513413+nagyjuciveronika99@users.noreply.github.com>
2018-02-18 20:54:31 +0100

    Add files via upload

---
+++ első.py
@@ -0,0 +1,55 @@
+def feladat_1(a,b):
+    a=a+b
+    b=a-b
+    a=a-b
+    print("a=",a)
+    print("b=",b)
+    
+def feladat_2(a,b,c):
+    if a<b and a<c
+        print(a)
+        if b>c:
+            print(c)
+            print(b)
+        else:
+            print(b)
+            print(c)
+    elif b<c and b<a:
+        print(b)
+        if a<c:
+            print(a)
+            print(c)
+        else:
+            print(c)
+            print(a)
+    else:
+        print(c)
+        if a<b:
+            print(a)
+            print(b)
+        else:
+            print(b)
+            print(a)
+            
+def feladat_3(x):
+    if x>-2 and x<0:
+        return 2*x
+    elif x>=0 and x<2:
+        return x*x
+    elif x>2:
+        return 10
+    else:
+        print("Nem eleme az ertelmezesi tartomanynak!")
+        return
+
+def feladat_5(a,b,c,d):
+    print(a,b,c,d)
+    if d>=0:
+        print(a,b,c,d)
+    else:
+        print(a,b,c,d)
+def feladat_4(a,b,c):
+    return min(a,b,c),(max(abs(a), abs(b), abs(c)))
+
+
+    
